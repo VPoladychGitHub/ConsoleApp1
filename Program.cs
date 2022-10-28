641db1462e90cd405843861f178363cb9993a631
VPoladychGitHub <vpoladych@ukr.net>
2022-10-28 20:35:27 +0300

    add args and calc lengh the input string

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-
+﻿
+string str = args[0].ToString();
+Console.WriteLine(str);
+Console.WriteLine(str.Length);
 
 //Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = Fal
